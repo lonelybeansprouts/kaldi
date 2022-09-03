e851127796163995f8a9db019fc973f0f3b0c927
dj <2238214883@qq.com>
2022-09-03 11:46:53 +0000

    udpate

--- egs/aishell/s5/run_bfctc_multiattention.sh
+++ egs/aishell/s5/run_bfctc_multiattention.sh
@@ -18,7 +18,7 @@ export OMP_NUM_THREADS=8  #set to batch_size
 export BFCTC_THREAD_NUM=12 #bfctc threads
 
 
-stage=4
+stage=5
 nj=32
 
 python_cmd="python3 -u"
@@ -44,7 +44,7 @@ data_type=raw
 checkpoint=
 average_checkpoint=true
 decode_checkpoint=$dir/final.pt
-average_num=10
+average_num=20
 
 
 echo "affix ${affix} start"
